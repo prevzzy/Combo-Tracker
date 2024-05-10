a3b34d84cd94d2d5ce743de33718bcca7c94f039
prevzzy <prevzzy@gmail.com>
2024-05-10 17:36:07 +0200

    adapt trick reading to reTHAWed

--- src/renderer/js/combo/trickHistory.js
+++ src/renderer/js/combo/trickHistory.js
@@ -1,6 +1,7 @@
 import { log } from '../debug/debugHelpers'
 import { getTrickColorClass } from '../ui/lastCombo/tricks/uiTricks'
 import * as MemoryController from '../game/memory'
+import { isTrackingRethawed } from '../game/interGameUtils'
 
 const TRICK_CONSTANTS = {
   SWITCH: 'Switch',
@@ -55,6 +56,10 @@ class Trick {
   Furthermore, there is an in-game 250 trickCount limit. Then, whenever a new trick is done, the in-game trick array moves its entire content a few indexes backward to make space for the latest tricks. Oldest tricks in the array are removed and new ones are appended.
   
   To avoid really complex logic and dealing with many edge cases, TrickHistory class mimicks the in-game trick history array. Given that there is quite a lot of logic based just on observations, saved trick history might not be 100% the same as the in-game trick history, although the differences should be rare and minimal.
+
+  trickCountNoGarbage is needed for failover THAW trick reading. This value tracks trick count differently from "normal" trickCount as it skips "empty" tricks (ollies, sketchy/clean landings and maybe some other stuff). F.e. if in THAW you are doing three tricks in a row mid-air, the "normal" trickCount gets updated after the second trick is made instead of the first one. The trickCountNoGarbage value however gets updated immediately for every trick. This means that the new trick might not always increment trickCount but will still overwrite the last trick in trick history array (always(?) an "empty" trick). Checking this value assures that no tricks are skipped in trick reading.
+    
+  This value can't be used to directly index the trickPointerArray though, as its value can go higher than 250 (the hardcoded in-game trickCount limit). I'm not sure what is this used for in-game, but it resets on combo bail and persists on combo land.
 */
 class TrickHistory {
   constructor() {
@@ -62,8 +67,9 @@ class TrickHistory {
     this.comboHistory = []
     this.trickPointerArray = []
     this.trickCount = 0
-    this.failedToReadTricksFlag = false
     this.gapsHit = 0
+    this.trickCountNoGarbage = 0
+    this.hasHitTrickCountLimit = false
   }
 
   updateTricksInCombo(newTricksArray) {
@@ -89,31 +95,60 @@ class TrickHistory {
 
   update() {
     const currentTrickCount = MemoryController.getTrickCount()
+    const currentTrickCountNoGarbage = MemoryController.getTrickCountWithNoGarbage()
 
-    this.handleNewTricksLogic(currentTrickCount)
+    this.handleNewTricks(currentTrickCount, currentTrickCountNoGarbage)
 
+    if (currentTrickCount === 250 && !this.hasHitTrickCountLimit) {
+      log('----TRICK LIMIT HIT----')
+      this.hasHitTrickCountLimit = true
+    }
+    
     this.trickCount = currentTrickCount
+    this.trickCountNoGarbage = currentTrickCountNoGarbage
   }
 
-  handleNewTricksLogic(currentTrickCount) {
+  handleNewTricks(currentTrickCount, currentTrickCountNoGarbage) {
     let trickToReadIndex = currentTrickCount - 1
     let trickToReadPointer = this.getTrickPointer(trickToReadIndex)
-    
+    const rescanLastTrickPointer = this.shouldRescanLastTrickPointer(currentTrickCount, currentTrickCountNoGarbage, trickToReadPointer)
+
     if (
       this.trickPointerArray.length !== 0 &&
-      this.trickPointerArray[this.trickPointerArray.length - 1] === trickToReadPointer
+      this.trickPointerArray[this.trickPointerArray.length - 1] === trickToReadPointer &&
+      !rescanLastTrickPointer
     ) {
-      // No new tricks
       return
     }
-      
-    this.adjustTrickPointerArrayStart()
-    const newTricksArray = this.readNewTricks(trickToReadIndex, trickToReadPointer)
 
-    this.comboHistory = [...this.comboHistory, ...newTricksArray]
-    this.updateTricksInCombo(newTricksArray)
+    this.adjustTrickPointerArrayStart()
+    const newTricksArray = this.readNewTricks(trickToReadIndex, trickToReadPointer, rescanLastTrickPointer)
+    this.updateTrickHistoryData(newTricksArray)
   }
 
+  shouldRescanLastTrickPointer(currentTrickCount, currentTrickCountNoGarbage, trickToReadPointer) {
+    if (
+      !isTrackingRethawed() ||
+      this.isNewTrickPointerAfterLimit(trickToReadPointer)
+    ) {
+      return false
+    }
+
+    const immediatelyUpdatedTrickCountDiff = currentTrickCountNoGarbage - this.trickCountNoGarbage
+    const trickCountDiff = currentTrickCount - this.trickCount
+
+    return immediatelyUpdatedTrickCountDiff > trickCountDiff
+  }
+
+  isNewTrickPointerAfterLimit(trickToReadPointer) {
+    // Stupid, arbitrary and naive but does the job. To assure that the app is not rescanning too many "same" pointer after 250 tricks an additional check for latest 15 tricks is made. If the trick pointer is new then it should be scanned.
+    return this.hasHitTrickCountLimit &&
+      this.trickPointerArray.length > 15 &&
+      !this.trickPointerArray
+        .slice(this.trickPointerArray.length - 15)
+        .find(pointer => pointer === trickToReadPointer)
+  }
+  
   adjustTrickPointerArrayStart() {
     let firstInGameTrick = this.getTrickPointer(1)
 
@@ -131,32 +166,26 @@ class TrickHistory {
       
     if (firstInGameTrickIndex !== -1) {
       this.trickPointerArray.splice(1, firstInGameTrickIndex - 1)
-    } else {
-      this.failedToReadTricksFlag = true
     }
   }
 
-  readNewTricks(firstTrickToReadIndex, firstTrickToReadPointer) {
+  readNewTricks(firstTrickToReadIndex, firstTrickToReadPointer, rescanLastTrickPointer) {
     let trickToReadIndex = firstTrickToReadIndex
     let trickToReadPointer = firstTrickToReadPointer
     const newTricksArray = []
 
-    // Could be multiple tricks done at a single frame so some extra logic is needed.
     while (trickToReadIndex >= 0) {
-      if (
-        this.trickPointerArray[trickToReadIndex] &&
-        this.trickPointerArray[trickToReadIndex] === trickToReadPointer
-      ) {
-        break
-      }
-      const newTrick = this.readSingleTrick(trickToReadPointer)
-      
-      if (newTrick) {
-        newTricksArray.push(newTrick)
-
-        if (newTrick.isGap()) {
-          this.gapsHit++
+      if (this.isTrickPointerScanned(trickToReadIndex, trickToReadPointer)) {
+        if (!rescanLastTrickPointer) {
+          break;
         }
+
+        rescanLastTrickPointer = false
+      }
+
+      const newTrick = this.readSingleTrick(trickToReadPointer)
+      if (newTrick) {
+        this.processNewTrick(newTricksArray, newTrick)
       }
       
       this.trickPointerArray[trickToReadIndex] = trickToReadPointer
@@ -172,6 +201,23 @@ class TrickHistory {
     }
 
     return newTricksArray
+  }
+
+  processNewTrick(newTricksArray, newTrick) {
+    newTricksArray.push(newTrick);
+
+    if (newTrick.isGap()) {
+      this.gapsHit++;
+    }
+  }
+
+  isTrickPointerScanned(index, pointer) {
+    return this.trickPointerArray[index] && this.trickPointerArray[index] === pointer;
+  }
+
+  updateTrickHistoryData(newTricksArray) {
+    this.comboHistory = [...this.comboHistory, ...newTricksArray]
+    this.updateTricksInCombo(newTricksArray)
   }
 
   readSingleTrick(trickDataPointer) {
@@ -209,7 +255,6 @@ class TrickHistory {
       }
     } catch(error) {
       console.error(error)
-      this.failedToReadTricksFlag = true
     }
   }
 
